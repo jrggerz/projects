45e31197bbc15f1339dc83274462b5fe5a1dac87
Jorge Ruiz <99931189+jrggerz@users.noreply.github.com>
2022-09-11 16:52:02 -0500

    node socket.io
    Implementacion de node socket io y socket io client dentro de react para mandar mensajes y se muestren mediante un arreglo

---
+++ node/chat-socket/client/src/App.js
@@ -0,0 +1,46 @@
+import './App.css';
+import io from 'socket.io-client';
+import { useState, useEffect } from 'react'
+//
+const socket = io('http://localhost:4000');
+//
+function App() {
+  const [message, setMessage] = useState('')
+  const [messages, setMessages] = useState([])
+  //
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    socket.emit('message', message);
+    const newMessage = {
+      body: message,
+      from: "Me"
+    }
+    setMessages([newMessage, ...messages])
+    setMessage('');
+  }
+  //
+  useEffect(() => {
+    const receiveMessage = (message) => {
+      setMessages([message, ...messages]);
+    };
+    socket.on('message', receiveMessage );
+    return () => socket.off('message', receiveMessage);
+  }, [messages]);
+  //
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={(e) => setMessage(e.target.value)}
+          value={message} />
+        <button>send</button>
+      </form>
+      {messages.map( (messages, index) =>(
+        <div key = {index}>
+          <p>{messages.from}: {messages.body}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+//
+export default App;
